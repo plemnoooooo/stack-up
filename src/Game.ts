353902dd1c9e8d6a6963bd220d8c523c55bf85db
plemno <loxican@outlook.sg>
2024-05-11 14:20:19 +0000

    functional and styled ui and proper restarting of game

--- src/Game.ts
+++ src/Game.ts
@@ -3,9 +3,6 @@ import $ from "jquery";
 import { clampOutside, getMeshSides, roundToNearest } from "./utils";
 
 export default class Game {
-    static readonly CANVAS_ID = "game-canvas";
-    static readonly SCORE_ID = "score";
-
     static readonly FOV = 60;
     static readonly NEAR = 0.1;
     static readonly FAR = 1000;
@@ -37,11 +34,16 @@ export default class Game {
     static readonly BACKGROUND_COLOR = 0x72bed6;
     static readonly KEY_TO_STACK_BLOCK = " ";
 
+    static readonly HOVER_DURATION = 300;
+    static readonly FINAL_SCORE_DELAY = 400;
+    static readonly FADE_DURATION = 800;
+    static readonly FADE_DELAY = 400;
+    static readonly END_SCREEN_FADE_IN = 800;
+    static readonly STOP_GAME_DELAY = 2000;
+
     gameOver: boolean;
     movement: number;
-    
-    canvas!: JQuery<HTMLCanvasElement>;
-    score!: JQuery<HTMLParagraphElement>;
+    score: number;
 
     scene!: THREE.Scene;
     renderer!: THREE.WebGLRenderer;
@@ -57,6 +59,7 @@ export default class Game {
     constructor() {
         this.gameOver = false;
         this.movement = 0;
+        this.score = 0;
         this.blocks = [];
 
         this.init();
@@ -64,25 +67,22 @@ export default class Game {
     }
 
     init() {
-        this.canvas = $(`#${Game.CANVAS_ID}`);
-        this.score = $(`#${Game.SCORE_ID}`);
-
         this.scene = new THREE.Scene();
-        this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas[0] });
+        this.renderer = new THREE.WebGLRenderer({ canvas: $("#game-canvas")[0] });
 
         const backgroundColor = new THREE.Color(Game.BACKGROUND_COLOR);
         this.scene.background = backgroundColor;
         $(document.body).css("background-color", `#${backgroundColor.getHexString()}`);
 
         this.renderer.setSize(window.innerWidth, window.innerHeight);
-        $(window).on("resize", this.onResize.bind(this));
+        $(window).on("resize", this.resize.bind(this));
 
         this.camera = new THREE.PerspectiveCamera(Game.FOV, window.innerWidth / window.innerHeight, Game.NEAR, Game.FAR);
         this.ambientLight = new THREE.AmbientLight();
         this.directionalLight = new THREE.DirectionalLight();
 
         this.camera.position.set(Game.CAMERA_X, Game.CAMERA_TRANSLATION_Y, Game.CAMERA_Z);
-        this.camera.lookAt(new THREE.Vector3());
+        this.camera.lookAt(Game.BLOCK_STARTING_WIDTH / 2, Game.BLOCK_HEIGHT, Game.BLOCK_STARTING_DEPTH / 2);
         this.camera.userData.targetY = Game.BLOCK_HEIGHT;
         this.camera.userData.targetZ = Game.CAMERA_Z;
 
@@ -104,7 +104,41 @@ export default class Game {
 
         this.scene.add(startingBlock, this.movingBlock, this.cutOffBlock);
 
-        $(window).on("keydown pointerdown", ({ type, key }) => !this.gameOver && !((type === "keydown") && (key !== Game.KEY_TO_STACK_BLOCK)) && this.stackBlock());
+        $(window).on("keydown pointerdown", ({ type, key }) => {
+            if (this.gameOver || ((type === "keydown") && (key !== Game.KEY_TO_STACK_BLOCK))) return;
+            this.stackBlock();
+
+            ($("#start-menu").css("opacity") === "1") && this.fadeWhenFirst();
+        });
+
+        $("#final-score").data({
+            showScore: false,
+            unrounded: 0
+        });
+
+        $("#reset").on("click", () => {
+            this.gameOver && this.reset();
+        }).on("pointerover", function () {
+            $(this).stop(true).css({
+                color: `#${Game.BACKGROUND_COLOR.toString(16)}`,
+                backgroundColor: "white"
+            }).animate({
+                width: "12.8rem",
+                height: "4.2rem",
+
+                borderRadius: "2.1rem",
+            }, 300);
+        }).on("pointerout", function () {
+            $(this).stop(true).css({
+                color: "white",
+                backgroundColor: "transparent"
+            }).animate({
+                width: "12rem",
+                height: "4rem",
+
+                borderRadius: "2rem"
+            }, 300);
+        });
     }
 
     animate() {
@@ -114,6 +148,10 @@ export default class Game {
         this.camera.position.y += ((this.camera.userData.targetY + Game.CAMERA_TRANSLATION_Y) - this.camera.position.y) / Game.CAMERA_DAMPING;
         this.camera.position.z += 1.6 * Math.sin((Math.PI / ((Game.CAMERA_Z - Game.CAMERA_GAME_OVER_Z) * 1.01)) * (this.camera.userData.targetZ - this.camera.position.z));
 
+        const unroundedScore = $("#final-score").data("unrounded");
+        $("#final-score").data("show-score") && $("#final-score").data("unrounded", unroundedScore + ((this.score / 30) * Math.sin(Math.PI / (this.score * 1.01) * (this.score - unroundedScore)))).text(Math.floor(roundToNearest($("#final-score").data("unrounded"), 0.1)));
+        $("#final-score").data("show-score") && console.log(unroundedScore)
+
         this.renderer.render(this.scene, this.camera);
         requestAnimationFrame(this.animate.bind(this));
     }
@@ -121,12 +159,22 @@ export default class Game {
     reset() {
         this.gameOver = false;
         this.movement = 0;
+        this.score = 0;
+
+        this.camera.userData.targetY = Game.BLOCK_HEIGHT;
+        this.camera.userData.targetZ = Game.CAMERA_Z;
 
         const removedBlocks = this.blocks.splice(1);
         this.scene.remove(...removedBlocks);
 
         this.movingBlock.visible = true;
+        this.movingBlock.userData.speed = Game.STARTING_MOVE_SPEED;
         this.resetMovingBlock();
+
+        $("#score").text(0);
+        $("#final-score").data("show-score", false);
+
+        this.fadeEndMenu(true);
     }
 
     stackBlock() {
@@ -149,8 +197,8 @@ export default class Game {
         const front = THREE.MathUtils.clamp(f1, b2, f2);
         const back = THREE.MathUtils.clamp(b1, b2, f2);
 
-        const width = right - left;
-        const depth = front - back;
+        const width = roundToNearest(right - left, Game.FIX_POSITION_VALUE);
+        const depth = roundToNearest(front - back, Game.FIX_POSITION_VALUE);
 
         this.resetCutOffBlock();
 
@@ -168,11 +216,12 @@ export default class Game {
         this.movement ^= 2;
         this.resetMovingBlock();
 
-        const scoreIncrement = (+(Object.is([l1, r1, f1, b1], [l2, r2, f2, b2])) * 2) + 1;
-        this.score.html(`${+this.score.html()! + scoreIncrement}`);
+        this.score += (+!this.cutOffBlock.material.opacity * 2) + 1;
 
         this.camera.userData.targetY = this.movingBlock.position.y;
         this.directionalLight.position.y = this.movingBlock.position.y + Game.LIGHT_TRANSLATION_Y;
+
+        $("#score").text(this.score);
     }
 
     updateMovingBlock() {
@@ -192,11 +241,12 @@ export default class Game {
         const isZMoving = !!(this.movement & 2);
         const moveMultiplier = (this.movement & 1) || -1;
 
-        const { left, right, back, front } = getMeshSides(this.blocks.slice(-1)[0]);
+        const mostTopBlock = this.blocks.slice(-1)[0];
+        const { left, right, back, front } = getMeshSides(mostTopBlock);
         const width = right - left;
         const depth = front - back;
 
-        this.moveBlock(this.movingBlock, isZMoving ? left : moveMultiplier * Game.BLOCK_STARTING_WIDTH, this.movingBlock.position.y + Game.BLOCK_HEIGHT, isZMoving ? moveMultiplier * Game.BLOCK_STARTING_DEPTH : back);
+        this.moveBlock(this.movingBlock, isZMoving ? left : moveMultiplier * Game.BLOCK_STARTING_WIDTH, mostTopBlock.position.y + Game.BLOCK_HEIGHT, isZMoving ? moveMultiplier * Game.BLOCK_STARTING_DEPTH : back);
         this.resizeBlock(this.movingBlock, width, depth);
 
         this.movingBlock.userData.speed += (Game.MAX_MOVE_SPEED - this.movingBlock.userData.speed) / Game.MOVE_DAMPING;
@@ -231,13 +281,13 @@ export default class Game {
         const front = clampOutside(f1, b2, f2, true);
         const back = clampOutside(b1, b2, f2);
 
-        const width = isZMoving ? this.movingBlock.geometry.parameters.width : right - left;
-        const depth = isZMoving ? front - back : this.movingBlock.geometry.parameters.depth;
+        const width = right - left;
+        const depth = front - back;
 
         this.moveBlock(this.cutOffBlock, isZMoving ? this.movingBlock.position.x : left, this.movingBlock.position.y, isZMoving ? back : this.movingBlock.position.z);
         this.resizeBlock(this.cutOffBlock, width, depth);
 
-        this.cutOffBlock.material.opacity = 1;
+        this.cutOffBlock.material.opacity = +!!(roundToNearest(width, Game.FIX_POSITION_VALUE) && roundToNearest(depth, Game.FIX_POSITION_VALUE));
     }
 
     createBlock(width: number, depth: number) {
@@ -274,13 +324,54 @@ export default class Game {
 
         setTimeout(() => {
             this.camera.userData.targetZ = Game.CAMERA_GAME_OVER_Z;
-        }, 2000);
+
+            setTimeout(() => {
+                this.fadeEndMenu();
+
+                $("#final-score").delay(Game.FADE_DELAY + Game.FINAL_SCORE_DELAY).data({
+                    showScore: true,
+                    unrounded: 0
+                });      
+            }, Game.END_SCREEN_FADE_IN);
+        }, Game.STOP_GAME_DELAY);
     }
 
-    onResize() {
+    resize() {
         this.camera.aspect = window.innerWidth / window.innerHeight;
         this.camera.updateProjectionMatrix();
 
         this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    fadeWhenFirst() {
+        $("#start-menu").animate({ opacity: 0 }, Game.FADE_DURATION, function () {
+            $(this).css("display", "none");
+        });
+
+        $("#score").delay(Game.FADE_DELAY).css("display", "inline").animate({ opacity: 1 }, Game.FADE_DURATION);
+    }
+
+    fadeEndMenu(fadeOut: boolean = false) {
+        $("#end-menu").stop(true).delay((1 - +fadeOut) * Game.FADE_DELAY).animate({ opacity: 1 - +fadeOut }, {
+            duration: Game.FADE_DURATION,
+            start() {
+                !fadeOut && $(this).css("display", "flex");
+            },
+
+            done() {
+                fadeOut && $(this).css("display", "none");
+            }
+        });
+
+        $("#score").stop(true).delay(+fadeOut * Game.FADE_DELAY).animate({ opacity: +fadeOut }, {
+            duration: Game.FADE_DURATION,
+            start() {
+                fadeOut && $(this).css("display", "inline");
+            },
+
+            done() {
+                !fadeOut && $(this).css("display", "none");
+            }
+        });
     }
 }
