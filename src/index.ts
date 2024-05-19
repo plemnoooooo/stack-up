99efc0d897c9c1fe6e2724a6c090d1d4ee26ef2b
plemno <loxican@outlook.sg>
2024-05-19 12:15:08 +0000

    remove eruda console

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,3 @@ import "./style.css";
 import Game from "./Game";
 
 window.addEventListener("DOMContentLoaded", () => new Game());
-
-(function () { var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.append(script); script.onload = function () { eruda.init(); } })();
